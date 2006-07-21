436873e227d92d4c01ca5092e747a0d73741cd15
Edward Z. Yang <edwardzyang@thewritingpot.com>
2006-07-21 23:27:00 +0000

    Move tester, complete HTML_Purifier -> HTMLPurifier move.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@68 48356398-32a2-884e-a903-53898d9a118a

---
+++ tests/index.php
@@ -0,0 +1,27 @@
+<?php
+
+load_simpletest(); // includes all relevant simpletest files
+
+// emulates inserting a dir called HTMLPurifier into your class dir
+set_include_path(get_include_path() . PATH_SEPARATOR . '../../');
+
+require_once 'XML/HTMLSax3.php'; // optional PEAR class
+
+// enforce proper namespacing
+require_once 'HTMLPurifier/HTMLPurifier.php';
+require_once 'HTMLPurifier/Lexer.php';
+require_once 'HTMLPurifier/Token.php';
+require_once 'HTMLPurifier/PureHTMLDefinition.php';
+require_once 'HTMLPurifier/Generator.php';
+
+$test = new GroupTest('HTMLPurifier');
+
+$test->addTestFile('HTMLPurifier.php');
+$test->addTestFile('Lexer.php');
+//$test->addTestFile('Token.php');
+$test->addTestFile('PureHTMLDefinition.php');
+$test->addTestFile('Generator.php');
+
+$test->run( new HtmlReporter() );
+
+?>
