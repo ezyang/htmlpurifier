9aa2da8f9f8b3de72d7f4dc60f7636744075c1ca
Edward Z. Yang <edwardzyang@thewritingpot.com>
2006-07-23 23:23:14 +0000

    Commit Strategy, which will soon be splitting up the Definition file.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@107 48356398-32a2-884e-a903-53898d9a118a

---
+++ library/HTMLPurifier/Strategy.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Supertype for classes that define a strategy for modifying/purifying tokens.
+ * 
+ * While HTMLPurifier's core purpose is fixing HTML into something proper, 
+ * strategies provide plug points for extra configuration or even extra
+ * features, such as custom tags, custom parsing of text, etc.
+ */
+
+class HTMLPurifier_Strategy
+{
+    
+    /**
+     * Executes the strategy on the tokens.
+     * 
+     * @param $tokens Array of HTMLPurifier_Token objects to be operated on.
+     * @returns Processed array of token objects.
+     */
+    function execute($tokens) {
+        trigger_error('Cannot call abstract function', E_USER_ERROR);
+    }
+    
+}
+
+?>
