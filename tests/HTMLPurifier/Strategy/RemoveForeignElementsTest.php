4c200f6bcbbd3f4c019c47cdac48debcedb98153
Edward Z. Yang <edwardzyang@thewritingpot.com>
2006-07-24 02:33:07 +0000

    Extract RemoveForeignElementsTest from Definition.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@116 48356398-32a2-884e-a903-53898d9a118a

---
+++ tests/HTMLPurifier/Strategy/RemoveForeignElementsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'HTMLPurifier/Strategy/RemoveForeignElements.php';
+
+class HTMLPurifier_Strategy_RemoveForeignElementsTest
+    extends HTMLPurifier_StrategyAbstractTest
+{
+    
+    function test() {
+        
+        $strategy = new HTMLPurifier_Strategy_RemoveForeignElements();
+        
+        $inputs = array();
+        $expect = array();
+        
+        $inputs[0] = '';
+        $expect[0] = $inputs[0];
+        
+        $inputs[1] = 'This is <b>bold text</b>.';
+        $expect[1] = $inputs[1];
+        
+        // [INVALID]
+        $inputs[2] = '<asdf>Bling</asdf><d href="bang">Bong</d><foobar />';
+        $expect[2] = htmlspecialchars($inputs[2]);
+        
+        foreach ($inputs as $i => $input) {
+            $tokens = $this->lex->tokenizeHTML($input);
+            $result_tokens = $this->strategy->execute($tokens);
+            $result = $this->gen->generateFromTokens($result_tokens);
+            $this->assertEqual($expect[$i], $result, "Test $i: %s");
+            paintIf($result, $result != $expect[$i]);
+        }
+        
+    }
+    
+}
+
+?>
