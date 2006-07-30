bba0d0b77a8415ad91e5ac733ac6b70baafcd128
Edward Z. Yang <edwardzyang@thewritingpot.com>
2006-07-30 16:01:24 +0000

    Remove Singleton functionality, the accumulator will just have to be passed around.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@130 48356398-32a2-884e-a903-53898d9a118a

--- library/HTMLPurifier/IDAccumulator.php
+++ library/HTMLPurifier/IDAccumulator.php
@@ -5,21 +5,9 @@ class HTMLPurifier_IDAccumulator
     
     var $ids = array();
     
-    function &instance() {
-        static $instance = null;
-        if (empty($instance)) {
-            $instance = new HTMLPurifier_IDAccumulator();
-        }
-        return $instance;
-    }
-    
     function add($id) {
         if (isset($this->ids[$id])) return false;
         return $this->ids[$id] = true;
-    }
-    
-    function reset() {
-        $this->ids = array();
     }
     
 }
--- tests/HTMLPurifier/IDAccumulatorTest.php
+++ tests/HTMLPurifier/IDAccumulatorTest.php
@@ -8,16 +8,11 @@ class HTMLPurifier_IDAccumulatorTest extends UnitTestCase
     function test() {
         
         // initialize the accumulator
-        $accumulator = HTMLPurifier_IDAccumulator::instance();
-        $accumulator->reset();
+        $accumulator = new HTMLPurifier_IDAccumulator();
         
         $this->assertTrue( $accumulator->add('id1'));
         $this->assertTrue( $accumulator->add('id2'));
         $this->assertFalse($accumulator->add('id1')); // repeated id
-        $accumulator->reset();
-        
-        $this->assertTrue( $accumulator->add('id2')); // test reset
-        $accumulator->reset();
         
     }
     
