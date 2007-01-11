dcaa374daef14510826f7dde6f03470afb24f1f4
Edward Z. Yang <edwardzyang@thewritingpot.com>
2007-01-11 22:37:54 +0000

    [1.3.3] Random miscellaneous housekeeping
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@636 48356398-32a2-884e-a903-53898d9a118a

--- docs/examples/basic.php
+++ docs/examples/basic.php
@@ -1,15 +1,14 @@
-<?php
+<?php exit;
 
 // This file demonstrates basic usage of HTMLPurifier.
 
-exit; // not to be called directly, it will fail fantastically!
-
-set_include_path('/path/to/htmlpurifier/library' . PATH_SEPARATOR . get_include_path());
-require_once 'HTMLPurifier.php';
+require_once '/path/to/htmlpurifier/library/HTMLPurifier.auto.php';
 
 $purifier = new HTMLPurifier();
 $html = '<b>Simple and short';
 
 $pure_html = $purifier->purify($html);
+
+echo $pure_html;
 
 ?>
