bd544ad0388354223c0dcb756a27088499c5bae6
Edward Z. Yang <edwardzyang@thewritingpot.com>
2007-02-09 03:19:43 +0000

    Formatting and documentation fixes.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@730 48356398-32a2-884e-a903-53898d9a118a

--- library/HTMLPurifier/ElementDef.php
+++ library/HTMLPurifier/ElementDef.php
@@ -9,6 +9,14 @@ class HTMLPurifier_ElementDef
     
     /**
      * Associative array of attribute name to HTMLPurifier_AttrDef
+     * @note Before being processed by HTMLPurifier_AttrCollections
+     *       when modules are finalized during
+     *       HTMLPurifier_HTMLDefinition->setup(), this array may also
+     *       contain an array at index 0 that indicates which attribute
+     *       collections to load into the full array. It may also
+     *       contain string indentifiers in lieu of HTMLPurifier_AttrDef,
+     *       see HTMLPurifier_AttrTypes on how they are expanded during
+     *       HTMLPurifier_HTMLDefinition->setup() processing.
      * @public
      */
     var $attr = array();
@@ -25,11 +33,7 @@ class HTMLPurifier_ElementDef
      */
     var $attr_transform_post = array();
     
-    /**
-     * Lookup table of tags that close this tag.
-     * @public
-     */
-    var $auto_close = array();
+    
     
     /**
      * HTMLPurifier_ChildDef of this tag.
@@ -38,20 +42,33 @@ class HTMLPurifier_ElementDef
     var $child;
     
     /**
-     * Abstract string representation of internal ChildDef rules
+     * Abstract string representation of internal ChildDef rules. See
+     * HTMLPurifier_ContentSets for how this is parsed and then transformed
+     * into an HTMLPurifier_ChildDef.
      * @public
      */
     var $content_model;
     
     /**
-     * Value of $child->type, used to determine which ChildDef to use
+     * Value of $child->type, used to determine which ChildDef to use,
+     * used in combination with $content_model.
      * @public
      */
     var $content_model_type;
     
+    
+    
+    /**
+     * Lookup table of tags that close this tag. Used during parsing
+     * to make sure we don't attempt to nest unclosed tags.
+     * @public
+     */
+    var $auto_close = array();
+    
     /**
      * Does the element have a content model (#PCDATA | Inline)*? This
-     * is important for chameleon ins and del processing.
+     * is important for chameleon ins and del processing in 
+     * HTMLPurifier_ChildDef_Chameleon.
      * @public
      */
     var $descendants_are_inline;
