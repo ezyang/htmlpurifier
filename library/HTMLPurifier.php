52fa958fb2572a71af0c25e132b935a2d0795b86
Edward Z. Yang <edwardzyang@thewritingpot.com>
2007-03-24 02:10:33 +0000

    Release 1.5.0 (bumped HTMLPurifier.php version number).
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@874 48356398-32a2-884e-a903-53898d9a118a

--- library/HTMLPurifier.php
+++ library/HTMLPurifier.php
@@ -22,7 +22,7 @@
  */
 
 /*
-    HTML Purifier 1.4.1 - Standards Compliant HTML Filtering
+    HTML Purifier 1.5.0 - Standards Compliant HTML Filtering
     Copyright (C) 2006 Edward Z. Yang
 
     This library is free software; you can redistribute it and/or
@@ -64,7 +64,7 @@ require_once 'HTMLPurifier/Encoder.php';
 class HTMLPurifier
 {
     
-    var $version = '1.4.1';
+    var $version = '1.5.0';
     
     var $config;
     var $filters;
