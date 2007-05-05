b3a599e8c216da6ed0777eea6587f710904826ac
Edward Z. Yang <edwardzyang@thewritingpot.com>
2007-05-05 22:40:51 +0000

    Add some more release scripts.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@1033 48356398-32a2-884e-a903-53898d9a118a

---
+++ svn.php
@@ -0,0 +1,14 @@
+<?php
+
+function svn_info($dir) {
+    $raw = explode("\n", shell_exec("svn info $dir"));
+    $svn_info = array();
+    foreach ($raw as $r) {
+        if (empty($r)) continue;
+        list($k, $v) = explode(': ', $r, 2);
+        $svn_info[$k] = $v;
+    }
+    return $svn_info;
+}
+
+?>
