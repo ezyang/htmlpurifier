831db14c790358246b663ac16430bf2e92a249e0
Edward Z. Yang <edwardzyang@thewritingpot.com>
2007-05-21 00:24:32 +0000

    [1.7.0] Remove HTMLModule tests. They were a bad idea.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@1083 48356398-32a2-884e-a903-53898d9a118a

--- tests/HTMLPurifier/HTMLModuleHarness.php
+++ tests/HTMLPurifier/HTMLModuleHarness.php
@@ -9,27 +9,6 @@ class HTMLPurifier_HTMLModuleHarness extends HTMLPurifier_StrategyHarness
         parent::setup();
         $this->obj = new HTMLPurifier_Strategy_Core();
     }
-    
-    function setupScaffold($module, $config = array()) {
-        
-        $this->config = HTMLPurifier_Config::create($config);
-        $this->config->set('HTML', 'AllowedModules', $module);
-        $def =& $this->config->getHTMLDefinition(true);
-        $def->manager->addModule(new HTMLPurifier_HTMLModuleHarness_Scaffold());
-        
-    }
-}
-
-/**
- * Special module that defines scaffolding for easy unit testing
- */
-class HTMLPurifier_HTMLModuleHarness_Scaffold extends HTMLPurifier_HTMLModule
-{
-    var $name = 'Scaffold';
-    var $attr_collections = array(
-        'Common' => array('ac:common' => 'Text'),
-        'Core'   => array('ac:core'   => 'Text')
-    );
 }
 
 ?>
