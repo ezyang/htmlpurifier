aec84dc3f64d6ba918069809ff68efbe9d41b4c5
Edward Z. Yang <edwardzyang@thewritingpot.com>
2007-05-28 03:58:02 +0000

    Simplify generate.php variable naming and comments.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@1108 48356398-32a2-884e-a903-53898d9a118a

--- configdoc/generate.php
+++ configdoc/generate.php
@@ -14,45 +14,24 @@ TODO:
 - determine how to multilingualize
 */
 
-// there are several hacks for the configForm.php smoketest
-// - load copies (override default schema)
-// - file/line server information hack
-// - post-processing, base-name change hack
-
-// ---------------------------------------------------------------------------
-// Check and configure environment
-
 if (version_compare('5', PHP_VERSION, '>')) exit('Requires PHP 5 or higher.');
 error_reporting(E_ALL); // probably not possible to use E_STRICT
 
-
-// ---------------------------------------------------------------------------
-// Include HTML Purifier library
-
+// load dual-libraries
 require_once '../library/HTMLPurifier.auto.php';
 require_once 'library/ConfigDoc.auto.php';
 
-
-// ---------------------------------------------------------------------------
-// Load copies of HTMLPurifier_ConfigDef and HTMLPurifier
-
 $schema = HTMLPurifier_ConfigSchema::instance();
-
-// ---------------------------------------------------------------------------
-// Generate final output using XSLT
-
-// determine stylesheet name
-$xsl_stylesheet_name = 'plain'; // use $_GET in the future
+$style = 'plain'; // use $_GET in the future
 $configdoc = new ConfigDoc();
-$html_output = $configdoc->generate($schema, $xsl_stylesheet_name);
+$output = $configdoc->generate($schema, $style);
 
-file_put_contents("$xsl_stylesheet_name.html", $html_output);
-
-// ---------------------------------------------------------------------------
-// Output for instant feedback
+// write out
+file_put_contents("$style.html", $output);
 
 if (php_sapi_name() != 'cli') {
-    echo $html_output;
+    // output = instant feedback
+    echo $output;
 } else {
     echo 'Files generated successfully.';
 }
