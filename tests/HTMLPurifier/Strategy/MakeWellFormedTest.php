62c6d93b6d1db23444c67520237a363b9f28a596
Edward Z. Yang <edwardzyang@thewritingpot.com>
2007-06-24 02:59:06 +0000

    Add more unit tests; everything seems to be good, but I'm suspicious.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@1217 48356398-32a2-884e-a903-53898d9a118a

--- tests/HTMLPurifier/Strategy/MakeWellFormedTest.php
+++ tests/HTMLPurifier/Strategy/MakeWellFormedTest.php
@@ -9,6 +9,7 @@ class HTMLPurifier_Strategy_MakeWellFormedTest extends HTMLPurifier_StrategyHarn
     function setUp() {
         parent::setUp();
         $this->obj = new HTMLPurifier_Strategy_MakeWellFormed();
+        $this->config = array();
     }
     
     function testNormalIntegration() {
@@ -172,6 +173,70 @@ Par
 
 ',
             '<p>Par</p>'
+        );
+        
+    }
+    
+    function testLinkify() {
+        
+        $this->config = array('Core.AutoLinkify' => true);
+        
+        $this->assertResult(
+            'http://example.com',
+            '<a href="http://example.com">http://example.com</a>'
+        );
+        
+        $this->assertResult(
+            '<b>http://example.com</b>',
+            '<b><a href="http://example.com">http://example.com</a></b>'
+        );
+        
+        $this->assertResult(
+            'This URL http://example.com is what you need',
+            'This URL <a href="http://example.com">http://example.com</a> is what you need'
+        );
+        
+    }
+    
+    function testMultipleInjectors() {
+        
+        $this->config = array('Core.AutoParagraph' => true, 'Core.AutoLinkify' => true);
+        
+        $this->assertResult(
+            'Foobar',
+            '<p>Foobar</p>'
+        );
+        
+        $this->assertResult(
+            'http://example.com',
+            '<p><a href="http://example.com">http://example.com</a></p>'
+        );
+        
+        $this->assertResult(
+            '<b>http://example.com</b>',
+            '<p><b><a href="http://example.com">http://example.com</a></b></p>'
+        );
+        
+        $this->assertResult(
+            '<b>http://example.com',
+            '<p><b><a href="http://example.com">http://example.com</a></b></p>'
+        );
+        
+        $this->assertResult(
+'http://example.com
+
+http://dev.example.com',
+            '<p><a href="http://example.com">http://example.com</a></p><p><a href="http://dev.example.com">http://dev.example.com</a></p>'
+        );
+        
+        $this->assertResult(
+            'http://example.com <div>http://example.com</div>',
+            '<p><a href="http://example.com">http://example.com</a></p><div><a href="http://example.com">http://example.com</a></div>'
+        );
+        
+        $this->assertResult(
+            'This URL http://example.com is what you need',
+            '<p>This URL <a href="http://example.com">http://example.com</a> is what you need</p>'
         );
         
     }
