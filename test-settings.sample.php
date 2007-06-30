4bc1761b123fe75beff8feea64c85a4e65e94dcc
Edward Z. Yang <edwardzyang@thewritingpot.com>
2007-06-30 05:02:27 +0000

    Update test settings file with more options.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@1302 48356398-32a2-884e-a903-53898d9a118a

--- test-settings.sample.php
+++ test-settings.sample.php
@@ -1,11 +1,18 @@
 <?php
 
-// This file is necessary to run the unit tests and profiling
-// scripts.
+// This file is necessary to run the unit tests and profiling scripts.
+// Please copy it to 'test-settings.php' and make the necessary edits.
+
+// It's recommended that you turn off PHP's time limit for the unit tests.
+set_time_limit(0);
+
+// Turning off output buffering will prevent mysterious errors from core dumps
+@ob_end_flush();
 
 // Is PEAR available on your system? If it isn't, set to false. If PEAR
 // is not part of the default include_path, add it.
-$GLOBALS['HTMLPurifierTest']['PEAR'] = true;
+$GLOBALS['HTMLPurifierTest']['PEAR'] = false;
+// set_include_path('/path/to/pear' . PATH_SEPARATOR . get_include_path());
 
 // How many times should profiling scripts iterate over the function? More runs 
 // means more accurate results, but they'll take longer to perform.
