d238b1a9ed9bd873346d21c52ae6f91f79e6a73b
Edward Z. Yang <edwardzyang@thewritingpot.com>
2008-02-04 21:57:55 +0000

    Temporary modification to auto until we get pure autoload working.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@1528 48356398-32a2-884e-a903-53898d9a118a

--- library/HTMLPurifier.auto.php
+++ library/HTMLPurifier.auto.php
@@ -8,3 +8,6 @@
 set_include_path(dirname(__FILE__) . PATH_SEPARATOR . get_include_path() );
 require_once 'HTMLPurifier/Bootstrap.php';
 require_once 'HTMLPurifier.autoload.php';
+
+// This is temporary until we get pure autoload working
+require_once 'HTMLPurifier.includes.php';
