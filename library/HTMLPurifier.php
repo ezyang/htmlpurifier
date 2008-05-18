587d6428265563afbc6b0e3eaf76d6fc1a19a560
Edward Z. Yang <edwardzyang@thewritingpot.com>
2008-05-18 05:46:06 +0000

    Release 3.1.0.
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@1728 48356398-32a2-884e-a903-53898d9a118a

--- library/HTMLPurifier.php
+++ library/HTMLPurifier.php
@@ -19,7 +19,7 @@
  */
 
 /*
-    HTML Purifier 3.1.0-dev - Standards Compliant HTML Filtering
+    HTML Purifier 3.1.0 - Standards Compliant HTML Filtering
     Copyright (C) 2006-2008 Edward Z. Yang
 
     This library is free software; you can redistribute it and/or
@@ -55,10 +55,10 @@ class HTMLPurifier
 {
     
     /** Version of HTML Purifier */
-    public $version = '3.1.0-dev';
+    public $version = '3.1.0';
     
     /** Constant with version of HTML Purifier */
-    const VERSION = '3.1.0-dev';
+    const VERSION = '3.1.0';
     
     /** Global configuration object */
     public $config;
