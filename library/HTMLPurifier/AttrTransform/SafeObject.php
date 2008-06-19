1009bd41a6c9a3302c9ffcc8c3cff51eadf2dfff
Edward Z. Yang <edwardzyang@thewritingpot.com>
2008-06-19 21:24:50 +0000

    var -> public
    git-svn-id: http://htmlpurifier.org/svnroot/htmlpurifier/trunk@1807 48356398-32a2-884e-a903-53898d9a118a

--- library/HTMLPurifier/AttrTransform/SafeObject.php
+++ library/HTMLPurifier/AttrTransform/SafeObject.php
@@ -5,7 +5,7 @@
  */
 class HTMLPurifier_AttrTransform_SafeObject extends HTMLPurifier_AttrTransform
 {
-    var $name = "SafeObject";
+    public $name = "SafeObject";
 
     function transform($attr, $config, $context) {
         if (!isset($attr['type'])) $attr['type'] = 'application/x-shockwave-flash';
