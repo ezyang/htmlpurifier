3759bfe77ec43121aee9097cfce1107cad8427fc
jbruni <joaobruni@brfree.com.br>
2009-03-31 22:09:22 -0700

    Setting 'name' attribute to its own definition

--- library/HTMLPurifier/HTMLModule/Name.php
+++ library/HTMLPurifier/HTMLModule/Name.php
@@ -9,7 +9,7 @@ class HTMLPurifier_HTMLModule_Name extends HTMLPurifier_HTMLModule
         $elements = array('a', 'applet', 'form', 'frame', 'iframe', 'img', 'map');
         foreach ($elements as $name) {
             $element = $this->addBlankElement($name);
-            $element->attr['name'] = 'ID';
+            $element->attr['name'] = 'Name';
         }
     }
 
