33a9807e69ae66e3e79c588265c44065aafa103f
jbruni <joaobruni@brfree.com.br>
2009-03-31 19:36:53 -0700

    Do not transform anchor name in ID

--- library/HTMLPurifier/HTMLModule/Tidy/Name.php
+++ library/HTMLPurifier/HTMLModule/Tidy/Name.php
@@ -14,8 +14,7 @@ class HTMLPurifier_HTMLModule_Tidy_Name extends HTMLPurifier_HTMLModule_Tidy
         // @name for img, a -----------------------------------------------
         // Technically, it's allowed even on strict, so we allow authors to use
         // it. However, it's deprecated in future versions of XHTML.
-        $r['img@name'] =
-        $r['a@name'] = new HTMLPurifier_AttrTransform_Name();
+        $r['img@name'] = new HTMLPurifier_AttrTransform_Name();
 
         return $r;
     }
