1b8c8865b2a743607a324048e571d40a3539af14
Edward Z. Yang <edwardzyang@thewritingpot.com>
2009-06-07 16:04:07 -0400

    Fix PHP 5.3.0 problem with numeric indices causing -0 problem. Signed-off-by: Edward Z. Yang <edwardzyang@thewritingpot.com>

--- library/HTMLPurifier/AttrDef/HTML/Class.php
+++ library/HTMLPurifier/AttrDef/HTML/Class.php
@@ -21,11 +21,14 @@ class HTMLPurifier_AttrDef_HTML_Class extends HTMLPurifier_AttrDef_HTML_Nmtokens
         foreach ($tokens as $token) {
             if (
                 ($allowed === null || isset($allowed[$token])) &&
-                !isset($forbidden[$token])
+                !isset($forbidden[$token]) &&
+                // We need this O(n) check because of PHP's array
+                // implementation that casts -0 to 0.
+                !in_array($token, $ret, true)
             ) {
-                $ret[$token] = true;
+                $ret[] = $token;
             }
         }
-        return array_keys($ret);
+        return $ret;
     }
 }
