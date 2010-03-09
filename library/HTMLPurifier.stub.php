6be498dbf8c12e39cf77f63afff431755a4623ee
Edward Z. Yang <ezyang@mit.edu>
2010-03-09 02:08:29 -0500

    Ini is not supported by HipHop.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- library/HTMLPurifier.stub.php
+++ library/HTMLPurifier.stub.php
@@ -3,6 +3,9 @@
 require_once 'HTMLPurifier.includes.php';
 
 $config = HTMLPurifier_Config::createDefault();
-if (isset($argv[1])) $config->loadIni($argv[1]);
+$config->set('Core.LexerImpl', 'DirectLex');
+for ($i = 1, $c = count($argv); $i < $c; $i += 2) {
+    $config->set($argv[$i], $argv[$i+1]);
+}
 $purifier = new HTMLPurifier($config);
 echo $purifier->purify(file_get_contents('php://stdin'));
