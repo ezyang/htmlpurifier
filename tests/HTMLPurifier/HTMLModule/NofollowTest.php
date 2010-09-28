8c80349f9dfd33b54947b95abdc026bdd6623374
Edward Z. Yang <ezyang@mit.edu>
2010-09-28 12:01:57 -0400

    Implement HTML.Nofollow for external links.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

---
+++ tests/HTMLPurifier/HTMLModule/NofollowTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class HTMLPurifier_HTMLModule_NofollowTest extends HTMLPurifier_HTMLModuleHarness
+{
+
+    function setUp() {
+        parent::setUp();
+        $this->config->set('HTML.Nofollow', true);
+    }
+
+    function testNofollow() {
+        $this->assertResult(
+            '<a href="http://google.com">a</a><a href="/local">b</a><a href="mailto:foo@example.com">c</a>',
+            '<a href="http://google.com" rel="nofollow">a</a><a href="/local">b</a><a href="mailto:foo@example.com">c</a>'
+        );
+    }
+
+}
+
+// vim: et sw=4 sts=4
