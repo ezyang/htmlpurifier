d1ec84f287bd623ebb05167d8a310a9925fa8842
Bradley M. Froehle <brad.froehle@gmail.com>
2011-02-14 10:45:48 -0800

    Add noframes option. Mimic img for height/width of iframe.
    According to specifications, width and height for iframe
    is Length, but for safety we treat it as Pixels unless
    HTML.MaxImgLength is disabled or HTML.Trusted is enabled.
    
    Signed-off-by: Bradley M. Froehle <brad.froehle@gmail.com>

--- library/HTMLPurifier/HTMLModule/SafeIframe.php
+++ library/HTMLPurifier/HTMLModule/SafeIframe.php
@@ -9,18 +9,28 @@ class HTMLPurifier_HTMLModule_SafeIframe extends HTMLPurifier_HTMLModule
     public $name = 'SafeIframe';
 
     public function setup($config) {
-
         $max = $config->get('HTML.MaxImgLength');
-        $embed = $this->addElement(
+
+        $iframe = $this->addElement(
             'iframe', 'Inline', 'Flow', 'Common',
             array(
                 'src*' => 'URI#embedded',
+                // According to the spec, it's Length, but percents can
+                // be abused, so we allow only Pixels.
                 'width' => 'Pixels#' . $max,
                 'height' => 'Pixels#' . $max,
                 'name' => 'ID',
                 'scrolling' => 'Enum#yes,no,auto',
                 'frameborder' => 'Enum#0,1',
             )
+        );
+        if ($max === null || $config->get('HTML.Trusted')) {
+            $iframe->attr['height'] =
+            $iframe->attr['width'] = 'Length';
+        }
+
+        $noframes = $this->addElement(
+            'noframes', 'Inline', 'Flow', 'Common'
         );
 
     }
