670514008286cf42d9b5e3974f96d8833918738b
Edward Z. Yang <ezyang@mit.edu>
2012-05-14 23:07:27 -0400

    Fix in AttrTransform_Nofollow
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- tests/HTMLPurifier/HTMLModule/NofollowTest.php
+++ tests/HTMLPurifier/HTMLModule/NofollowTest.php
@@ -6,18 +6,19 @@ class HTMLPurifier_HTMLModule_NofollowTest extends HTMLPurifier_HTMLModuleHarnes
     function setUp() {
         parent::setUp();
         $this->config->set('HTML.Nofollow', true);
+        $this->config->set('Attr.AllowedRel', array("nofollow", "blah"));
     }
 
     function testNofollow() {
         $this->assertResult(
-            '<a href="http://google.com">a</a><a href="/local">b</a><a href="mailto:foo@example.com">c</a>',
-            '<a href="http://google.com" rel="nofollow">a</a><a href="/local">b</a><a href="mailto:foo@example.com">c</a>'
+            '<a href="http://google.com">x</a><a href="http://google.com" rel="blah">a</a><a href="/local">b</a><a href="mailto:foo@example.com">c</a>',
+            '<a href="http://google.com" rel="nofollow">x</a><a href="http://google.com" rel="blah nofollow">a</a><a href="/local">b</a><a href="mailto:foo@example.com">c</a>'
         );
     }
 
     function testNofollowDupe() {
         $this->assertResult(
-            '<a href="http://google.com" rel="nofollow">a</a><a href="/local">b</a><a href="mailto:foo@example.com">c</a>'
+            '<a href="http://google.com" rel="nofollow">x</a><a href="http://google.com" rel="blah nofollow">a</a><a href="/local">b</a><a href="mailto:foo@example.com">c</a>'
         );
     }
 
