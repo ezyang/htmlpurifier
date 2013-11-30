6f389f0f25b90d0b495308efcfa073981177f0fd
Edward Z. Yang <ezyang@cs.stanford.edu>
2013-11-30 00:25:19 -0800

    Release 4.6.0.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- library/HTMLPurifier/Config.php
+++ library/HTMLPurifier/Config.php
@@ -21,7 +21,7 @@ class HTMLPurifier_Config
      * HTML Purifier's version
      * @type string
      */
-    public $version = '4.5.0';
+    public $version = '4.6.0';
 
     /**
      * Whether or not to automatically finalize
