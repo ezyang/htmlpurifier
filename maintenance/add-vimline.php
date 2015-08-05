ae1828d955112356f7677c465f94f7deb7d27a40
Edward Z. Yang <ezyang@cs.stanford.edu>
2015-08-04 18:03:42 -0700

    Release 4.7.0.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- maintenance/add-vimline.php
+++ maintenance/add-vimline.php
@@ -35,7 +35,6 @@ foreach ($files as $file) {
         postfix_is('.phpt', $file) ||
         postfix_is('VERSION', $file) ||
         postfix_is('WHATSNEW', $file) ||
-        postfix_is('FOCUS', $file) ||
         postfix_is('configdoc/usage.xml', $file) ||
         postfix_is('library/HTMLPurifier.includes.php', $file) ||
         postfix_is('library/HTMLPurifier.safe-includes.php', $file) ||
