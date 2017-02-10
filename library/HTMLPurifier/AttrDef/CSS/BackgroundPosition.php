d9980a3e75c4d7c02e5aaa5afb72338c7f0de732
f.godfrin <f.godfrin@fairpost.fr>
2017-02-10 22:04:41 +0100

    Revert Background position changes

--- library/HTMLPurifier/AttrDef/CSS/BackgroundPosition.php
+++ library/HTMLPurifier/AttrDef/CSS/BackgroundPosition.php
@@ -54,16 +54,10 @@ class HTMLPurifier_AttrDef_CSS_BackgroundPosition extends HTMLPurifier_AttrDef
      */
     protected $percentage;
 
-    /**
-     * @type HTMLPurifier_AttrDef_CSS_Position
-     */
-    protected $position;
-
     public function __construct()
     {
         $this->length = new HTMLPurifier_AttrDef_CSS_Length();
         $this->percentage = new HTMLPurifier_AttrDef_CSS_Percentage();
-        $this->position = new HTMLPurifier_AttrDef_CSS_Position();
     }
 
     /**
@@ -76,12 +70,6 @@ class HTMLPurifier_AttrDef_CSS_BackgroundPosition extends HTMLPurifier_AttrDef
     {
         $string = $this->parseCDATA($string);
         $bits = explode(' ', $string);
-
-        // check if the whole string is a valid position
-        $r = $this->position->validate($string, $config, $context);
-        if ($r !== false) {
-            return $r;
-        }
 
         $keywords = array();
         $keywords['h'] = false; // left, right
@@ -106,9 +94,9 @@ class HTMLPurifier_AttrDef_CSS_BackgroundPosition extends HTMLPurifier_AttrDef
             }
 
             // test for keyword
-            $r = $this->position->validate($bit, $config, $context);
-            if ($r !== false) {
-                $status = $lookup[$r];
+            $lbit = ctype_lower($bit) ? $bit : strtolower($bit);
+            if (isset($lookup[$lbit])) {
+                $status = $lookup[lbit];
                 if ($status == 'c') {
                     if ($i == 0) {
                         $status = 'ch';
@@ -116,7 +104,7 @@ class HTMLPurifier_AttrDef_CSS_BackgroundPosition extends HTMLPurifier_AttrDef
                         $status = 'cv';
                     }
                 }
-                $keywords[$status] = $r;
+                $keywords[$status] = $lbit;
                 $i++;
             }
 
