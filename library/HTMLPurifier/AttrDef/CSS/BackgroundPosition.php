f87681e124bed3eb9eb2989388047df2d1a067fa
f.godfrin <f.godfrin@fairpost.fr>
2017-02-10 22:05:33 +0100

    Add missing $

--- library/HTMLPurifier/AttrDef/CSS/BackgroundPosition.php
+++ library/HTMLPurifier/AttrDef/CSS/BackgroundPosition.php
@@ -96,7 +96,7 @@ class HTMLPurifier_AttrDef_CSS_BackgroundPosition extends HTMLPurifier_AttrDef
             // test for keyword
             $lbit = ctype_lower($bit) ? $bit : strtolower($bit);
             if (isset($lookup[$lbit])) {
-                $status = $lookup[lbit];
+                $status = $lookup[$lbit];
                 if ($status == 'c') {
                     if ($i == 0) {
                         $status = 'ch';
