ce7efc11b20b47bc7f287ee66ef02b6cd4b6fce2
Edward Z. Yang <ezyang@mit.edu>
2020-06-28 20:38:16 -0400

    Delete language tests that are interfering with PSR-0 compatibility
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- tests/HTMLPurifier/LanguageFactoryTest.php
+++ tests/HTMLPurifier/LanguageFactoryTest.php
@@ -29,45 +29,6 @@ class HTMLPurifier_LanguageFactoryTest extends HTMLPurifier_Harness
 
     }
 
-    public function testFallback()
-    {
-        $this->config->set('Core.Language', 'en-x-test');
-        $language = $this->factory->create($this->config, $this->context);
-
-        $this->assertIsA($language, 'HTMLPurifier_Language_en_x_test');
-        $this->assertIdentical($language->code, 'en-x-test');
-
-        $language->load();
-
-        // test overloaded message
-        $this->assertIdentical($language->getMessage('HTMLPurifier'), 'HTML Purifier X');
-
-        // test inherited message
-        $this->assertIdentical($language->getMessage('LanguageFactoryTest: Pizza'), 'Pizza');
-
-    }
-
-    public function testFallbackWithNoClass()
-    {
-        $this->config->set('Core.Language', 'en-x-testmini');
-        $language = $this->factory->create($this->config, $this->context);
-        $this->assertIsA($language, 'HTMLPurifier_Language');
-        $this->assertIdentical($language->code, 'en-x-testmini');
-        $language->load();
-        $this->assertIdentical($language->getMessage('HTMLPurifier'), 'HTML Purifier XNone');
-        $this->assertIdentical($language->getMessage('LanguageFactoryTest: Pizza'), 'Pizza');
-        $this->assertIdentical($language->error, false);
-    }
-
-    public function testNoSuchLanguage()
-    {
-        $this->config->set('Core.Language', 'en-x-testnone');
-        $language = $this->factory->create($this->config, $this->context);
-        $this->assertIsA($language, 'HTMLPurifier_Language');
-        $this->assertIdentical($language->code, 'en-x-testnone');
-        $this->assertIdentical($language->error, true);
-    }
-
 }
 
 // vim: et sw=4 sts=4
