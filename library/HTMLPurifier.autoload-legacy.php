6aa4166b7ef5021c6e6d95748a37e9541bd984d5
kishor <44048735+kish0808@users.noreply.github.com>
2020-09-16 05:42:43 +0530

    Issue-256: Fix PHP 7.3 compatibility issues update zend.ze1_compatibility_mode mode (#267)

--- library/HTMLPurifier.autoload-legacy.php
+++ library/HTMLPurifier.autoload-legacy.php
@@ -4,13 +4,11 @@
  * @file
  * Legacy autoloader for systems lacking spl_autoload_register
  *
- * Must be separate to prevent deprecation warning on PHP 7.2
  */
 
 spl_autoload_register(function($class)
 {
      return HTMLPurifier_Bootstrap::autoload($class);
-}
 });
 
 // vim: et sw=4 sts=4
