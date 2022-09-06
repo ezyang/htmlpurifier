32417eaa8bf5de98a02327bcc93dd7d15e8696ae
Kieran Brahney <kieran@supportpal.com>
2022-09-06 16:41:42 +0100

    Fix creation of dynamic property

--- tests/HTMLPurifier/DefinitionTestable.php
+++ tests/HTMLPurifier/DefinitionTestable.php
@@ -1,7 +1,14 @@
 <?php
 
+abstract class HTMLPurifier_TestDefinition extends HTMLPurifier_Definition
+{
+    public $info;
+    public $info_candles;
+    public $info_random;
+}
+
 Mock::generatePartial(
-        'HTMLPurifier_Definition',
+        'HTMLPurifier_TestDefinition',
         'HTMLPurifier_DefinitionTestable',
         array('doSetup'));
 
