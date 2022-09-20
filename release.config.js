e55fead09f39430d30f48438f06e7bc2326efc94
Kieran <kieran@supportpal.com>
2022-09-20 17:45:11 +0100

    fix: semantic release (#341)
    Same as #339 but stops library/standalone and library/HTMLPurifier.standalone.phpfrom being commit

--- release.config.js
+++ release.config.js
@@ -4,26 +4,30 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', {'changelogFile': 'NEWS'}],
-    '@semantic-release/exec',
-    ['@semantic-release/git', {
-      'assets': ['VERSION', 'NEWS', 'Doxyfile', 'library/HTMLPurifier.php', 'library/HTMLPurifier/Config.php', 'library/HTMLPurifier.includes.php'],
-    }],
+    [
+      '@semantic-release/changelog',
+      {
+        'changelogFile': 'NEWS'
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        'prepareCmd': 'php update-for-release ${nextRelease.version}'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        'assets': [
+          'VERSION',
+          'NEWS',
+          'Doxyfile',
+          ['library/**/*', '!library/standalone/**/*', '!library/HTMLPurifier.standalone.php'],
+          'configdoc/**/*',
+        ],
+      }
+    ],
     '@semantic-release/github'
   ],
-  verifyConditions: [
-    '@semantic-release/changelog',
-    '@semantic-release/github',
-  ],
-  prepare: [
-    {
-      path: '@semantic-release/exec',
-      cmd: 'php update-for-release ${nextRelease.version}'
-    },
-    '@semantic-release/changelog',
-    '@semantic-release/git',
-  ],
-  publish: [
-    '@semantic-release/github',
-  ]
 }
