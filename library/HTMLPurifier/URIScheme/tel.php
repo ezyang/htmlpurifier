8429f7b5071412d63da3ef3970474cac4d9b2855
danbrellis <danbrellis@gmail.com>
2023-11-09 23:43:30 -0500

    Allows commas in tel URI scheme validator (addresses #388)

--- library/HTMLPurifier/URIScheme/tel.php
+++ library/HTMLPurifier/URIScheme/tel.php
@@ -35,7 +35,7 @@ class HTMLPurifier_URIScheme_tel extends HTMLPurifier_URIScheme
 
         // Delete all non-numeric characters, non-x characters
         // from phone number, EXCEPT for a leading plus sign.
-        $uri->path = preg_replace('/(?!^\+)[^\dx]/', '',
+        $uri->path = preg_replace('/(?!^\+)[^\dx,]/', '',
                      // Normalize e(x)tension to lower-case
                      str_replace('X', 'x', rawurldecode($uri->path)));
 
