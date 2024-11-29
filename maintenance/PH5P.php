9db0d35a3595a61c28ee89582639fdd68ff3ce85
Viktor Szépe <viktor@szepe.net>
2024-11-29 12:42:04 +0100

    Deprecate HTML5TreeConstructer

--- maintenance/PH5P.php
+++ maintenance/PH5P.php
@@ -1139,7 +1139,14 @@ class HTML5
     }
 }
 
-class HTML5TreeConstructor
+class HTML5TreeConstructor extends HTML5TreeConstructer
+{}
+
+/**
+ * @deprecated Use HTML5TreeConstructor instead
+ * @todo Remove with version 5
+ */
+class HTML5TreeConstructer
 {
     public $stack = array();
 
